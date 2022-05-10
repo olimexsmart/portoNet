41a81067b5b09ade973e9d2b993fe4072e045cad
Luca Olivieri <gliolivieri@live.it>
2022-05-10 21:38:36 +0200

    Added another opening method

--- API/doorOpen.php
+++ API/doorOpen.php
@@ -2,8 +2,12 @@
 
 function openDoor()
 {
+/*  // Valid if on RaspberryPi
     system("gpio mode 1 out");
     system("gpio write 1 1");
     sleep(1);
     system("gpio write 1 0");
+*/
+
+  file_get_contents("http://192.168.1.33/open");
 }
